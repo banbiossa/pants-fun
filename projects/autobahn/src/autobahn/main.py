8440469627c9a83e9ee6e2d8d085a7fbce710a2b
Shota Shimizu <shota_shimizu@caddi.jp>
2022-11-03 20:07:14 +0900

    add more stuff

--- projects/autobahn/src/autobahn/main.py
+++ projects/autobahn/src/autobahn/main.py
@@ -1,2 +1,20 @@
+import logging
+import os
+
+import pandas as pd
+from autobahn.finder import find_files
+
+logger = logging.getLogger(__name__)
+
+
 def main():
+    files = os.listdir(".")
+    python_files = find_files(files, ".py")
+    logger.info(python_files)
     return "I am autobahn"
+
+
+def return_df() -> pd.DataFrame:
+    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+    logger.info(df)
+    return df
--- projects/autobahn/tests/test_main.py
+++ projects/autobahn/tests/test_main.py
@@ -1,5 +1,13 @@
-from autobahn.main import main
+import pandas as pd
+from autobahn.main import main, return_df
 
 
 def test_main():
     assert main() == "I am autobahn"
+
+
+def test_return_df():
+    df = return_df()
+    assert df.shape == (3, 2)
+    assert isinstance(df, pd.DataFrame)
+    pd.testing.assert_frame_equal(df, pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}))
